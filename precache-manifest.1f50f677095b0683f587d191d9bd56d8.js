1a2a11ce97fa8e1ba2e67b4ee2d1f1f3a3ceecc1
ssaleem <sara@sarasaleem.com>
2019-05-01 20:04:41 -0400

    Updates

---
+++ precache-manifest.1f50f677095b0683f587d191d9bd56d8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30da5ed86ec932b7b7e374433916330a",
+    "url": "/Cheese-SPA/index.html"
+  },
+  {
+    "revision": "f84e2fb9c53f598585ab",
+    "url": "/Cheese-SPA/static/css/main.5db3891e.chunk.css"
+  },
+  {
+    "revision": "d91873aef8ee2bf3a027",
+    "url": "/Cheese-SPA/static/js/2.8fb29255.chunk.js"
+  },
+  {
+    "revision": "f84e2fb9c53f598585ab",
+    "url": "/Cheese-SPA/static/js/main.ae29c983.chunk.js"
+  },
+  {
+    "revision": "aebca5817265a888d172",
+    "url": "/Cheese-SPA/static/js/runtime~main.b9823201.js"
+  }
+]);
