362a47b541ecd25fe62b35f9bd8fc2b6831529d9
ssaleem <sara@sarasaleem.com>
2019-05-09 13:00:37 -0400

    Add category API functionality for fetching all categories and adding new category

---
+++ src/api/categoryApi.js
@@ -0,0 +1,23 @@
+import {API_URL, handleResponse} from './index';
+
+const RESOURCE = 'categories';
+
+export function getCategories () {
+  return fetch (`${API_URL}/${RESOURCE}/`).then (res => res.json ());
+}
+
+export function addCategory (category) {
+  return fetch (`${API_URL}/${RESOURCE}/`, {
+    method: 'post',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify (category),
+  })
+    .then (res => {
+      console.log (res.headers.get ('Location'));
+      return res.json ();
+    })
+    .then (result => handleResponse (result));
+}
