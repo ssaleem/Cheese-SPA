61aeac70a9cda0103293f368e61de83a7655f4da
ssaleem <sara@sarasaleem.com>
2019-05-10 15:47:15 -0400

    Add routes and wrap app for router and MUI

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
-import './App.css';
+import {Route} from 'react-router-dom';
+import Cheese from './components/cheese';
+import Category from './components/category';
+import Menu from './components/menu';
+import NavBar from './components/Navbar';
+import Main from './components/Main';
+import Footer from './components/Footer';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
-}
+const App = () => (
+  <>
+    <NavBar/>
+    <Route exact path="/" component={Main} />
+    <Route path="/cheeses" component={Cheese} />
+    <Route path="/categories" component={Category} />
+    <Route path="/menus" component={Menu} />
+    <Footer/>
+  </>
+)
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter} from 'react-router-dom';
+import {MuiThemeProvider} from '@material-ui/core/styles';
+import theme from './theme';
 import './index.css';
 import App from './App';
 
-
-ReactDOM.render(<App />, document.getElementById('root'));
-
+ReactDOM.render (
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <MuiThemeProvider theme={theme}>
+      <App />
+    </MuiThemeProvider>
+  </BrowserRouter>,
+  document.getElementById ('root')
+);
