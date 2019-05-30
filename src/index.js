6651c96def0cdd3eaf6d2d600c5a637a69c6daa2
ssaleem <sara@sarasaleem.com>
2019-05-30 14:31:15 -0400

    Replace BrowserRouter with HashRouter to allow local routing

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter} from 'react-router-dom';
+import {HashRouter} from 'react-router-dom';
 import {MuiThemeProvider} from '@material-ui/core/styles';
 import theme from './theme';
 import './index.css';
 import App from './App';
 
 ReactDOM.render (
-  <BrowserRouter basename={process.env.PUBLIC_URL}>
+  <HashRouter basename={process.env.PUBLIC_URL}>
     <MuiThemeProvider theme={theme}>
       <App />
     </MuiThemeProvider>
-  </BrowserRouter>,
+  </HashRouter>,
   document.getElementById ('root')
 );
